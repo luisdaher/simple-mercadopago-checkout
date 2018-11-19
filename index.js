3f6a08617ddf0cc5f801ff9a4d21f6d45a3f13e5
Luis Daher <luisotaviodaher@gmail.com>
2018-11-19 21:26:26 -0200

    adding additional params

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 
 import { NativeModules } from 'react-native'
+import axios from 'axios'
 
 const { RNSimpleMercadopagoCheckout } = NativeModules
 
 export default RNSimpleMercadopagoCheckout
 
-export function startCheckout (publicKey, checkoutPreferenceId) {
+export function startCheckout (clientId, clientSecret, publicKey, email = 'demo@mail.com', items = []) {
   console.log('nativemodules')
   console.log(NativeModules.RNSimpleMercadopagoCheckout)
+  if (!clientId) throw 'clientId required to start MercadoPago checkout'
+  if (!clientSecret) throw 'clientSecret required to start MercadoPago checkout'
   if (!publicKey) throw 'publicKey required to start MercadoPago checkout'
-  if (!checkoutPreferenceId) throw 'checkoutPreferenceId required to start MercadoPago checkout'
 
-  return RNSimpleMercadopagoCheckout.startCheckout(publicKey, checkoutPreferenceId)
+  return axios.post('https://api.mercadopago.com/oauth/token', {
+    grant_type: 'client_credentials',
+    client_id: clientId,
+    client_secret: clientSecret
+  }).then(function (response) {
+    // handle success
+    // console.log(response.data.access_token)
+    let accessToken = response.data.access_token
 
+    return axios.post(`https://api.mercadopago.com/checkout/preferences?access_token=${accessToken}`, {
+      items: items,
+      payer: { email: email }
+    }).then(function (res) {
+      // handle success
+      // console.log(res)
+      let checkoutPreferenceId = res.data.id
+      return RNSimpleMercadopagoCheckout.startCheckout(publicKey, checkoutPreferenceId)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+      return new Promise((resolve, reject) => reject(error))
+    })
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error)
+    return new Promise((resolve, reject) => reject(error))
+  })
 }
